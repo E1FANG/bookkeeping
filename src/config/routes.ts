fb614d37116f75a11fc020fca2ab71b6254dabd9
fanghs <fanghs@citycloud.com.cn>
2022-06-16 10:31:51 +0800

    修改默认路由

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -3,15 +3,13 @@ import { First } from "../components/welcome/first";
 import { Forth } from "../components/welcome/forth";
 import { Second } from "../components/welcome/second";
 import { Third } from "../components/welcome/third";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/Bar', component: Bar },
+  { path: '/', redirect:'/welcome' },
   {
     path: '/welcome', component: Welcome, children: [
+      {path:'',redirect:'/welcome/1'},
       { path: '1', component:First},
       { path: '2', component:Second},
       { path: '3', component:Third},
