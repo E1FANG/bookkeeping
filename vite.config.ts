08bc4f6d1832908cf6a77bf3a09aa8480cb8586f
fanghs <fanghs@citycloud.com.cn>
2022-06-16 11:58:50 +0800

    feat:移动浏览器全屏样式

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bookkeeping/dist/',
+  // base: '/bookkeeping/dist/',
   plugins: [
     vue(), 
     vueJsx({
